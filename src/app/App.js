1b0799a0fc03493c636aee5c095139d83941a14e
BIT Student <classroom@bgit.rs>
2019-06-24 10:20:32 +0200

    start

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export class App extends React.Component {
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '../src/app/App';
+import { BrowserRouter } from 'react-router-dom'
 
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));

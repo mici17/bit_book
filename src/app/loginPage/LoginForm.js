a3e97e1933c44bf4c4c68c782cf64dc606539fdf
BIT Student <classroom@bgit.rs>
2019-06-24 16:36:40 +0200

    tokenlogin

--- src/app/loginPage/LoginForm.js
+++ src/app/loginPage/LoginForm.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './LoginForm.css'
 
 export const LoginForm = (props) => {
-    console.log('click')
+
     return (
         <div className="login-form-container">
             <div className="login-cards">
                 <span className="login-login-card" onClick={props.onLoginClickChange}><a href="#">Login</a></span>
                 <span className="login-register-card" onClick={props.onRegisterClickChange}><a href="#">Register</a></span>
             </div>
-            <form className="login-form"  >
+            <form className="login-form">
                 <label>email
-                    <input type="email" placeholder="Email Address" value="" className="login-input" />
+                    <input type="email" name="email" placeholder="Email Address" value={props.email} className="login-input" onChange={props.onInputChange} />
                 </label>
                 <label>pass
-              <input type="password" placeholder="Password" value="" className="login-input" />
+              <input type="password" name="password" placeholder="Password" value={props.password} className="login-input" onChange={props.onInputChange} />
                 </label>
-                <button className="login-button">Login</button>
+                <button className="login-button" onClick={props.onSubmitLogIn}>Login</button>
             </form>
         </div >
     )
--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -4,30 +4,30 @@ const authenticationLogIn = (email, password) => {
     return fetch(('https://book-api.hypetech.xyz/v1/auth/login'), {
         method: 'POST',
         headers: {
-            'Content-Type': 'application-json',
+            'Content-Type': 'application/json',
             'x-api-key': 'B1tD3V'
         },
-        body: {
+        body: JSON.stringify({
             email: email,
             password: password
-        }
+        })
     })
         .then((response) => response.json())
 }
 
 
 const authenticationRegister = (name, email, password) => {
-    (('https://book-api.hypetech.xyz/v1/auth/register'), {
+    return fetch(('https://book-api.hypetech.xyz/v1/auth/register'), {
         method: 'POST',
         headers: {
-            'Content-Type': 'application-json',
+            'Content-Type': 'application/json',
             'x-api-key': 'B1tD3V'
         },
-        body: {
+        body: JSON.stringify({
             name: name,
             email: email,
             password: password
-        }
+        })
 
     })
         .then((response) => response.json())

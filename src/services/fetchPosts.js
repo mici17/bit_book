2a92c19d1861f13706a93f65203177755c6c6dff
BIT Student <classroom@bgit.rs>
2019-06-25 15:59:09 +0200

    feed done

--- src/services/fetchPosts.js
+++ src/services/fetchPosts.js
@@ -1,7 +1,17 @@
 import { Post } from '../entities/Post'
 
-export const fetchPosts = () => {
-    return fetch("http://book-api-dev.hypetech.xyz/posts")
+export const fetchPosts = (token) => {
+    return fetch(("http://book-api-dev.hypetech.xyz/posts"), {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'B1tD3V',
+            'Authorization': 'Bearer ' + token
+        },
+    }
+
+
+    )
         .then(response => response.json())
         .then(posts => {
             return posts.map((post, i) => {

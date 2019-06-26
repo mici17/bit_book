165faaeed04e70ac3e513de6494881170ac60365
Milica <mici17>
2019-06-27 00:09:09 +0200

    users page done

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import { FeedPage } from './FeedPage/FeedPage'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { FeedPage } from './FeedPage/FeedPage';
+import {UsersPage} from'./UsersPage/UsersPage';
 
 
 export class Main extends React.Component {
@@ -8,7 +9,9 @@ export class Main extends React.Component {
         return (
             <main>
                 <Switch>
+                    {/* <Route path="/people/:id" component={SingleUser}/> */}
                     <Route path="/feed" component={FeedPage} />
+                    <Route path="/people" component={UsersPage}/>
                 </Switch>
             </main>
         )
---
+++ src/services/fetchUsers.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    User
+} from '../entities/User';
+
+export const fetchUsers = () => {
+    return fetch((`https://book-api.hypetech.xyz/v1/users`), {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': 'B1tD3V',
+                    'Authorization': `Bearer ${localStorage.getItem("loginToken")}`
+                },
+            }
+
+
+        )
+        .then(response => response.json())
+        .then(users => {
+            return users.map(user =>
+                new User(user))
+        })
+
+}
